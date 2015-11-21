f714761bdd26c3e6182d5b6900e800c41748eef9
mlding <mlding@thoughtworks.com>
2015-11-21 13:39:12 +0800

    [Mengli]change delete style.

--- src/js/components/ItemRow.jsx
+++ src/js/components/ItemRow.jsx
@@ -3,35 +3,40 @@ var TodoAction = require('../actions/TodoAction.jsx');
 
 var ItemRow = React.createClass({
 
-    getInitialState: function() {
+    getInitialState: function () {
         return {
-            item: this.props.item
+            item: this.props.item,
+            delete: false
         }
     },
 
-    componentWillReceiveProps: function(nextProps) {
+    componentWillReceiveProps: function (nextProps) {
         this.setState({
             item: nextProps.item
         })
     },
 
-    onChange: function(event) {
+    onChange: function (event) {
         this.setState({
             item: event.target.value
         })
     },
 
     deleteIcon: function (event) {
-        TodoAction.deleteItem(this.props.item);
+        //TodoAction.deleteItem(this.props.item);
+        this.setState({
+            delete: true
+        });
     },
 
     render: function () {
         return (
             <div>
                 <input type="checkbox"/>
-                <input value={this.state.item} onChange={this.onChange}/>
+                <input value={this.state.item} onChange={this.onChange}
+                       className={this.state.delete ? "delete-item" : " "}/>
                 <span className="delete-icon" onClick={this.deleteIcon}>&nbsp;&nbsp;
-                delete</span>
+                    delete</span>
             </div>
         );
     }
